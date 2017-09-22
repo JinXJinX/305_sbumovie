cc6f92aa8c14893aec79ab047f7113963c45e74c
Xu Jing <jingxu086@gmail.com>
2017-09-22 03:53:55 -0400

    update

--- project/config.py
+++ project/config.py
@@ -2,9 +2,8 @@
 
 import os
 CSRF_ENABLED = True
-SECRET_KEY = '780316'
+SECRET_KEY = ''
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-#SQLALCHEMY_DATABASE_URI = 'mysql+pymysql:///' + os.path.join(basedir, 'app.db')
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:781022@localhost:3306/CSE305?charset=utf8mb4'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://user:password@localhost:3306/CSE305?charset=utf8mb4'
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
